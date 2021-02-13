33bbb54e706aa08ce1f1db98c6c2ee2692f60884
rstudio019 <rstudio019@gmail.com>
2021-02-13 22:52:01 +0700

    jobeet day 9 done

--- src/Command/CreateCategoryCommand.php
+++ src/Command/CreateCategoryCommand.php
@@ -2,13 +2,26 @@
 
 namespace App\Command;
 
+use App\Service\CategoryService;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
 
 class CreateCategoryCommand extends Command
 {
+    /**
+     * @var CategoryService
+     */
+    private $categoryService;
+
+    public function __construct(CategoryService $categoryService)
+    {
+        $this->categoryService = $categoryService;
+        parent::__construct();
+    }
+    
     protected function configure()
     {
         $this
@@ -25,11 +38,37 @@ class CreateCategoryCommand extends Command
             ->addArgument('name', InputArgument::REQUIRED, 'The name of the category.');
     }
 
+    /** 
+     * This method is executed after initialize() and before execute(). Its purpose is to check if some of the options/arguments are missing and interactively ask the user for those values. This is the last place where you can ask for missing options/arguments. After this command, missing options/arguments will result in an error.
+    */
+    protected function interact(InputInterface $input, OutputInterface $output)
+    {
+        if(!$input->getArgument('name')){
+            /*
+            Symfony has some predefined tags: <info>, <comment>, <question> and <error>. Wrap our question in proper style:
+            */
+            $question = new Question('<question>Please choose a name:</question>');
+            $question->setValidator(function ($name) {
+                if(empty($name)){
+                    throw new \Exception('Name can not be empty');
+                }
+
+                return $name;
+            });
+
+            $answer = $this->getHelper('question')->ask($input,$output,$question);
+            $input->setArgument('name',$answer);
+        }
+    }
+
     /**
      *
      * @param InputInterface $input
      * @param OutputInterface $output
      * @return void
+     */
+    /**
+     * This method is executed after interact() and initialize(). It contains the logic you want the command to execute.
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
@@ -37,5 +76,9 @@ class CreateCategoryCommand extends Command
         $output->writeln(['Category Creator','=============','']);
 
         $output->writeln(sprintf('Name: %s',$input->getArgument('name')));
+
+        $this->categoryService->create($input->getArgument('name'));
+
+        $output->writeln('<fg=red;bg=yellow;options=bold>category successfully created</>');
     }
 }
